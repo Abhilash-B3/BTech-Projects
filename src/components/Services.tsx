7640a3121bad3dfa247d24a9ce2308ffdcc5bc7d
Abhilash-B3 <abhilash17.it@gmail.com>
2026-01-26 20:36:12 +0530

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -5,7 +5,7 @@ import { Service } from '../types';
 const services: Service[] = [
   {
     title: 'BTech Projects',
-    price: '₹800 – 3000+',
+    price: 'Custom Projects',
     description:'We build all types of projects: Web Development, Full-Stack, Deep Learning, and Machine Learning',
     features: [
       'Source Code',
